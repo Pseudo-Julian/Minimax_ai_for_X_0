9dc71da424c33ee13333d5b3d4a608516da92a86
Pseudo-Julian <julianweter@gmail.com>
2025-09-17 23:28:34 +0300

    create func player, winner, terminal

--- tictactoe.py
+++ tictactoe.py
@@ -22,7 +22,13 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    count_of_x = sum(cell == X for row in board for cell in row)
+    count_of_o = sum(cell == O for row in board for cell in row)
+
+    if count_of_x == 0 or count_of_o == count_of_x:
+        return X
+    else:
+        return O
 
 
 def actions(board):
@@ -43,14 +49,56 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    sum_of_x = [[], [], [], [], []]  # [0][3]-firs diagonal, [0][4]-second diagonal
+    sum_of_o = [[], [], [], [], []]  # ^
+    n1, n2 = 0, 0
+
+    for row in board:
+        n2 -= 1
+        if sum(cell == X for cell in row) == 3:
+            return X
+        if sum(cell == O for cell in row) == 3:
+            return O
+
+        for i in range(3):
+            if row[i] == X:
+                sum_of_x[i].append(1)
+            if row[i] == O:
+                sum_of_o[i].append(1)
+        for j in range(3):
+            if sum(sum_of_x[j]) == 3:
+                return X
+            if sum(sum_of_o[j]) == 3:
+                return O
+
+        if row[n1] == X:
+            sum_of_x[3].append(1)
+        if row[n2] == X:
+            sum_of_x[4].append(1)
+        if row[n1] == O:
+            sum_of_o[3].append(1)
+        if row[n2] == O:
+            sum_of_o[4].append(1)
+        n1 += 1
+
+        if sum(sum_of_x[3]) == 3 or sum(sum_of_x[4]) == 3:
+            return X
+        if sum(sum_of_o[3]) == 3 or sum(sum_of_o[4]) == 3:
+            return O
+
+    if sum(cell == EMPTY for row in board for cell in row) == 0:
+        return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    count_of_empty = sum(cell == EMPTY for row in board for cell in row)
+    if count_of_empty == 0 or winner(board) in (X, O):
+        return True
+    else:
+        return False
 
 
 def utility(board):
